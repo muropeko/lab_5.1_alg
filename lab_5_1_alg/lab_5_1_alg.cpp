450f04289468aef7df091e40146c4f1254134cb8
Anastasiia Dovhoshiya <Dovgoshiya.uwo@gmail.com>
2022-10-28 09:50:29 +0300

    first commit

---
+++ lab_5_1_alg/lab_5_1_alg.cpp
@@ -0,0 +1,36 @@
+﻿/*#include <iostream>
+#include <cmath>
+using namespace std;
+double h(const double x, const double y); 
+int main()
+{
+	double s, t;
+	
+	cout << "s = "; cin >> s;
+	cout << "t = "; cin >> t;
+	double c = h(pow(s,2),pow(t,2)) + h(pow(s+t,1),2);
+	cout << "c = " << c << endl;
+	return 0;
+}
+double h(const double x, const double y) 
+{
+	return ((x * y) / 1 + pow(x, 2) * pow(y, 2));
+}*/
+#include <iostream>
+#include <cmath>
+using namespace std;
+double h(const double x, const double y, const double z); // прототип
+int main()
+{
+	double a, b;
+	
+		cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	double c = h(a, -b, 1) + h(2, a + b, abs(a - b));
+	cout << "c = " << c << endl;
+	return 0;
+}
+double h(const double x, const double y, const double z) // визначення
+{
+	return (x * x + x * z - y * z + y * y) / (x * x + abs(x * y * z) + z * z);
+}
---
+++ lab_5_3/lab_5_3.cpp
@@ -0,0 +1,48 @@
+﻿// lab_5_3.cpp
+// Довгошиї Анастасії
+// Лабораторна робота № 5.3
+//  Функції, що містять розгалуження та цикли з рекурентними співвідношеннями
+// Варіант 6
+
+#include <iostream>
+
+#include <cmath>
+using namespace std;
+double j(const double x);
+int main()
+{
+	double kp, kk, z;
+	int n;
+	cout << "kp = "; cin >> kp;
+	cout << "kk = "; cin >> kk;
+	cout << "n = "; cin >> n;
+	double kg = (kk - kp) / n;
+	double k = kp;
+	while (k <= kk)
+	{
+		z = j(k) + pow(j(k-1), 2) + 2*j(1);
+		cout << k << " " << z << endl;
+		k += kg;
+	}
+	return 0;
+}
+double j(const double x)
+{
+	if (abs(x) >= 1)
+		return (sin(x)+1)/(pow(cos(x),2) + exp(x));
+	else
+	{
+		double S = 0;
+		int k = 0;
+		double a = 1;
+		S = a;
+		do
+		{
+			k++;
+			double R = (2*x*(2*k-1)*(2*k))/(2*k);
+			a *= (1/cos(2*x)) * R;
+			S += a;
+		} while (k < 7);
+		return S;
+	}
+}
